09763f5c10ac2d46756cedba48aa06cd2c0a3b02
DudiP <dudi@timetable.co.il>
2019-07-31 16:29:46 +0300

    add title

--- angular/index.ts
+++ angular/index.ts
@@ -1,25 +1,17 @@
 import {registerElement} from 'nativescript-angular/element-registry';
-import {Component, OnInit, ViewChild, NgModule,  EventEmitter, Output} from "@angular/core";
+import {Component, OnInit, ViewChild, NgModule,  EventEmitter, Output, Input} from "@angular/core";
 import {DateRange} from "../src";
 registerElement("DateRange", () => require("../src").DateRange);
 
 @Component({
-    // tslint:disable-next-line:max-line-length directive-selector
     selector: "date-range-component",
-    template: `
-    <StackLayout class="page" height="100%" backgroundColor="#ccc">
-    <StackLayout height="10%">
-       <Button text="Test Btn" (tap)="selectedDate($event)"></Button>
-    </StackLayout>
-     <StackLayout> 
-       <DateRange  #elm> </DateRange> 
-     </StackLayout>
-    </StackLayout>
-    `,
-
+    templateUrl: './date-range-component.html',
+    styleUrls: ['./date-range-component.css'],
 })
 export class DateRangeComponent implements OnInit {
     // @ts-ignore
+    @Input() titleText;
+    @Input() buttonText;
     @ViewChild('elm', {static: true}) elm: DateRange;
     @Output() onSelectedDate: EventEmitter<any> = new EventEmitter<any>();
     private _androidViewId: number;
